620f8ad4b2c3f2531eb505d1a624852ef7e6be0e
Jayaprakashs-AdapptLimited <jayaprakash.s@adappt.ai>
2024-04-12 14:54:17 +0530

    initial commit

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var character = 'mario';
+console.log(character);
+var userInputs = document.querySelectorAll('input');
+userInputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const character = 'mario';
+
+console.log(character);
+
+const userInputs = document.querySelectorAll('input');
+
+userInputs.forEach(input => {
+  console.log(input);
+});
