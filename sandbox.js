ddd8afe9aac472c87ee107477886dbad998fa081
Jayaprakashs-AdapptLimited <jayaprakash.s@adappt.ai>
2024-04-12 18:40:35 +0530

    explicit type

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,26 @@
-var character = 'mario';
-console.log(character);
-var userInputs = document.querySelectorAll('input');
-userInputs.forEach(function (input) {
-    console.log(input);
-});
+/////////// EXPLICIT TYPES
+var character;
+var age;
+var isBoolean;
+// not assignable to type string
+// character = 14;
+character = 'steve';
+console.log(character, 'character');
+isBoolean = false;
+console.log(isBoolean, 'bool');
+/////// Arrays
+var arrays = [];
+// arrays = [45,77];
+arrays = ['tim', 'michael'];
+console.log(arrays);
+var mixed = [];
+mixed.push('arr');
+mixed.push(8);
+// cannot add bool value
+// mixed.push(true);
+console.log(mixed);
+/// OBJECTS
+var userName;
+userName = { name: 'bhuvi', wickets: 22 };
+// array is a type of object
+// userName = ['dff'];
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,39 @@
-const character = 'mario';
+/////////// EXPLICIT TYPES
 
-console.log(character);
+let character: string;
+let age: number;
+let isBoolean: boolean;
 
-const userInputs = document.querySelectorAll('input');
+// not assignable to type string
+// character = 14;
+character = 'steve';
+console.log(character, 'character');
+isBoolean = false;
 
-userInputs.forEach(input => {
-  console.log(input);
-});
+console.log(isBoolean, 'bool');
+
+/////// Arrays
+
+let arrays: string[] = [];
+
+// arrays = [45,77];
+arrays = ['tim', 'michael'];
+console.log(arrays);
+
+
+let mixed: (string | number)[] = [];
+mixed.push('arr');
+mixed.push(8);
+// cannot add bool value
+// mixed.push(true);
+
+console.log(mixed);
+
+/// OBJECTS
+
+let userName: object;
+
+userName = {name: 'bhuvi', wickets: 22};
+// array is a type of object
+// userName = ['dff'];
+
