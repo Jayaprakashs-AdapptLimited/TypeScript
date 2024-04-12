d59704f2070bfcb4a3010302ef629f5ca36553cc
Jayaprakashs-AdapptLimited <jayaprakash.s@adappt.ai>
2024-04-12 15:28:40 +0530

    type string and number

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,11 @@
-var character = 'mario';
-console.log(character);
-var userInputs = document.querySelectorAll('input');
-userInputs.forEach(function (input) {
-    console.log(input);
-});
+var users = 'Dhan';
+// cannot assign users value of string to number 
+// users = 14;
+var age = 22;
+// cannot assign users value of number to string 
+// age = 'ffjkef';
+////////// 
+// const circ = (diameter) => diameter * Math.PI;
+// console.log(circ(5));
+var circl = function (diameter) { return diameter * Math.PI; };
+console.log(circl(5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,18 @@
-const character = 'mario';
+let users = 'Dhan';
 
-console.log(character);
+// cannot assign users value of string to number 
+// users = 14;
 
-const userInputs = document.querySelectorAll('input');
+let age = 22;
+// cannot assign users value of number to string 
+// age = 'ffjkef';
 
-userInputs.forEach(input => {
-  console.log(input);
-});
+
+////////// 
+// const circ = (diameter) => diameter * Math.PI;
+
+// console.log(circ(5));
+
+const circl = (diameter : number) => diameter * Math.PI;
+
+console.log(circl(5));
